e3e57c281fb80c06d563166ef2193dedce72ec60
zhangqingjian <mingrui-521@163.com>
2017-07-14 10:51:24 +0800

    2

--- Cloud-Eureka-client-provider-mybaits/src/main/java/com/yo/news/test/MainApplication.java
+++ Cloud-Eureka-client-provider-mybaits/src/main/java/com/yo/news/test/MainApplication.java
@@ -6,7 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.client.RestTemplate;
+@EnableTransactionManagement
 @EnableAutoConfiguration
 @EnableDiscoveryClient
 @SpringBootApplication
